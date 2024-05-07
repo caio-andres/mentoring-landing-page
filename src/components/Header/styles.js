382e9a4a6aada8c5a56b0e48ecd1905b5a8f097e
Caio André <caioandresgit@outlook.com>
2024-05-07 19:20:58 -0300

    feat(Header): Responsive created, font changed.

---
+++ src/components/Header/styles.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+import breakpoint from "../../styles/breakpoints";
+
+export const MenuHide = styled.a`
+  @media ${breakpoint.lg} {
+    display: none;
+  }
+`;
+
+export const MenuHideAbout = styled.a`
+  @media ${breakpoint.md} {
+    display: none;
+  }
+`;
+
+export const MenuHideStart = styled.nav`
+  @media ${breakpoint.xs} {
+    display: none !important;
+  }
+`;
+
+export const DivResponsive = styled.div`
+  @media ${breakpoint.sm} {
+    margin: 1px !important;
+  }
+`;
+
+
---
+++ src/styles/breakpoints.js
@@ -0,0 +1,15 @@
+const size = {
+    xs: '605px',
+    sm: '674px',
+    md: '992px',
+    lg: '1282px',
+}
+
+const breakpoint = {
+    xs: `(max-width: ${size.xs})`,
+    sm: `(max-width: ${size.sm})`,
+    md: `(max-width: ${size.md})`,
+    lg: `(max-width: ${size.lg})`,
+}
+
+export default breakpoint;
