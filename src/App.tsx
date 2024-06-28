d4595782afce3fa47846dbb1c92052a702a333bd
Caio André <caioandresgit@outlook.com>
2024-06-27 21:46:20 -0300

    fix(Programming Language): Project changed to TypeScript at all.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import RoutesApp from "./routes.tsx";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <RoutesApp />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./styles/global.css";
+
+const rootElement = document.getElementById("root");
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.error("Failed to find the root element.");
+}
